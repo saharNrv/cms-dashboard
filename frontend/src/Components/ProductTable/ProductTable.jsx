3150708d2d91b1e1171e9c89ef9fa92592daeea2
saharNrv <122102473+saharNrv@users.noreply.github.com>
2023-12-31 21:03:04 +0330

    finish productTable style

---
+++ frontend/src/Components/ProductTable/ProductTable.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './ProductTable.css'
+
+export default function ProductTable() {
+  return (
+    <div className=' m-5'>
+
+        <table className='product-table'>
+            <thead>
+                <tr className='table-heading-tr'>
+                    <th>Cover</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Count</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className='table-body-tr'>
+                    <td>
+                        <img src="/img/lap1.jpeg" alt="product img" className='w-[120px] object-cover' />
+                    </td>
+                    <td>laptop samsung</td>
+                    <td>120000$</td>
+                    <td>12</td>
+                    <td>
+                        <button className='bg-teal-500 w-[80px] mx-2 p-2 text-white rounded-md'>Delete</button>
+                        <button className='bg-teal-500 w-[80px] mx-2 p-2 text-white rounded-md'>Edite</button>
+                        <button className='bg-teal-500 w-[80px] mx-2 p-2 text-white rounded-md'>Detaile</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+      
+    </div>
+  );
+}
--- frontend/src/Components/Products/Products.jsx
+++ frontend/src/Components/Products/Products.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Product.css'
 import Message from '../Message/Message';
 import AddNewProduct from '../AddNewProduct/AddNewProduct';
+import ProductTable from '../ProductTable/ProductTable';
 
 export default function Products() {
   return (
     <div className=''>
      <Message msg='No products found'/>
      <AddNewProduct/>
+     <ProductTable/>
     </div>
   );
 }
